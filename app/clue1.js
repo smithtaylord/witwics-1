bead5098add984541312edd85acf370a19719e29
Taylor Smith <smith.taylord@gmail.com>
2023-01-25 19:48:50 -0700

    solved clue 1

--- app/clue1.js
+++ app/clue1.js
@@ -3,7 +3,7 @@
 // A message sent to her associate requires we decode it using a letter within a certain position of a string. 
 // Ex: ('Cryptology', 3) should return 'p'
 function letterAtPosition(str, position) {
-  // TODO YOUR CODE HERE
+  return str[position]
 }
 
 
@@ -11,19 +11,20 @@ function letterAtPosition(str, position) {
 
 // Write a function that will join two strings with an optional seperating character 
 function concat(str1, str2, seperator = " ") {
-  // TODO YOUR CODE HERE
+  return str1 + seperator + str2
 }
 
 // Our cryptologists have discovered that some of the letters that were seperated out should also be capitalized.
 
 // Write a function that will capitalize the letter at a given position and return it
 function capitalizer(str, position) {
-  // TODO YOUR CODE HERE
+  let upperCase = str[position].toUpperCase()
+  return upperCase
 }
 
 
 // There are a few key words we need to look for to find if they are in the message
 // write a function that will take in two strings and identify if the first string 'includes' the second
 function messageContainsPhrase(message, phrase) {
-  // TODO YOUR CODE HERE
+  return message.includes(phrase)
 }
